ee28363fc2eec07e8474538206500d10982ed9a7
Oleg Cherkashin <impuls_64@mail.ru>
2022-12-13 21:12:22 +0300

    setuptools

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
-# Это уже нам знакомое callable WSGI-приложение
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Oleg!'
+    return 'Welcome Oleg!'
